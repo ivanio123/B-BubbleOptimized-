03b3403728ac62f4911f415043b3f34a12916405
whigga_02 <jodsonjod@gmail.com>
2020-03-16 18:23:10 +0200

    Добавьте файлы проекта.

---
+++ SortTest_VS2010/StudSort.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#if VALUE_IS_DOUBLE
+	using ValueType = System.Double;
+#else
+#if VALUE_IS_SHORT
+	using ValueType = System.Int16;
+#else
+#if VALUE_IS_LONG
+	using ValueType = System.Int64;
+#else
+using ValueType = System.Int32;
+#endif
+#endif
+#endif
+
+namespace SortTest
+{
+    class StudSort
+    {
+        // You may add more method(s) here, if needed 
+        // (f.e., for recursive quicksort or recursive mergesort)
+
+        public static ValueType[] Sort(ValueType[] data) // DON'T CHANGE this line!!!
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                bool isChanged = false;
+                for (int j = 0; j < data.Length - 1 - i; j++)
+                {
+                    if (data[j] > data[j + 1])
+                    {
+                        ValueType temp = data[j];
+                        data[j] = data[j + 1];
+                        data[j + 1] = temp;
+                        isChanged = true;
+                    }
+                }
+                if (isChanged == false)
+                    break;
+            }
+            return data;
+        }
+    }
+}
+
